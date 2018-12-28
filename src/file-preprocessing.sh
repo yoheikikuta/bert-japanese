c26955558e6b538dae1a94f67f534c16e43d73f4
Yohei KIKUTA <yohei@Yoheis-MacBook.local>
2018-12-28 23:47:25 +0900

    Add more unix commands for approrpiate text preprocessing

--- src/file-preprocessing.sh
+++ src/file-preprocessing.sh
@@ -5,15 +5,18 @@ CURDIR=$(cd $(dirname $0); pwd)
 source <(grep TEXTDIR ${CURDIR}/../config.ini | sed 's/ *= */=/g')
 
 # Text preprocessing.
-# 1. Remove blank lines.
-# 2. Remove <doc id ... line and its next line (title of an article).
-# 3. Replace </doc> line with a blank line.
-# 4. Break line at each 。 (but not 。」 or 。）...) position.
-# 5. Remove lines with the head 。(these lines are not meaningful).
-# 6. Convert upper case characters to lower case ones.
+# 1-1. Remove blank lines.
+# 1-2. Remove <doc id ... line and its next line (title of an article).
+# 1-3. Replace </doc> line with a blank line.
+# 2-1. Remove spaces at the end of each line.
+# 2-2. Break line at each 。, but not at 。」 or 。）, position.
+# 2-3. Remove spaces at the head of each line.
+# 3. Remove lines with the head 。(these lines are not meaningful).
+# 4. Convert upper case characters to lower case ones.
 for FILE in $( find ${TEXTDIR} -name "wiki_*" ); do
     echo "Processing ${FILE}"
-    sed -i -e '/^$/d; /<doc id/,+1d; s/<\/doc>//g; s/。\([^」|)|）|"]\)/。\n\1/g' ${FILE}
+    sed -i -e '/^$/d; /<doc id/,+1d; s/<\/doc>//g' ${FILE}
+    sed -i -e 's/ *$//g; s/。\([^」|)|）|"]\)/。\n\1/g; s/^[ ]*//g' ${FILE}
     sed -i -e '/^。/d' ${FILE}
     sed -i -e 's/\(.*\)/\L\1/' ${FILE}
 done
